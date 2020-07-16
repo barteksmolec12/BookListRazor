288b28dc77ee87171c1313bce5a4943a2aab0fc2
barteksmolec12 <barteksmolec@o2.pl>
2020-07-16 20:23:33 +0200

    connection with database

--- BookListRazor/Model/Book.cs
+++ BookListRazor/Model/Book.cs
@@ -10,10 +10,10 @@ namespace BookListRazor.Model
 	{
 		[Key]
 
-		public int Id { get; set; }
+		public int Id{ get; set; }
 		[Required]
 		public string Name { get; set; }
-		public string Author { get; set; }
+		public string Author { get; set; } 
 
 	}
 }
